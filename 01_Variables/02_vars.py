86213df15064d2cf08563bec545d24d4bf4ce778
Praveen MP <praveen.mpp007@gmail.com>
2020-04-12 02:51:05 +0530

    assignment, re-assignment

---
+++ 01_Variables/02_vars.py
@@ -0,0 +1,54 @@
+# Variables ---
+# Assigning Var --
+# 02
+message = "Hello World!"
+print(message)
+# O/p:
+# Hello Python world!
+
+
+# Reassignment with the same var --
+# 03
+message = "Hello Python World!"
+print(message)
+# O/p:
+# Hello Python world!
+# Hello Python Crash Course world!
+
+# Another Reassignment example -
+a = 1000
+print("a =", a)
+a += a
+print("a =", a)
+a += a
+print("a =", a)
+# O/p:
+# a = 1000
+# a = 2000
+# a = 4000
+
+
+# Avoiding Name Errors When Using Variables --
+# 04: error
+# message = "Hello Python Crash Course reader!"
+# print(mesage)
+# O/p:
+# NameError: name 'mesage' is not defined
+
+# 05
+mesage = "Hello Python Crash Course reader!"
+print(mesage)
+# O/p:
+# "Hello Python Crash Course reader!"
+
+
+# Simple Encryption and Decryption Example
+message = 111
+hash_code = 346728236
+secret_message = message * hash_code
+print("secret_message:", secret_message)
+# secret_message: 38486834196
+
+decrypted_message = secret_message / hash_code
+print("decrypted_message:", decrypted_message)
+# decrypted_message: 111.0
