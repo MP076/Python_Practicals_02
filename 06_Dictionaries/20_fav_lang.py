134ae01c6c90e4581eaf86ea1abc4ff5db1aa19b
Praveen MP <praveen.mpp007@gmail.com>
2020-04-12 21:50:09 +0530

    dictionary of similar objects

---
+++ 06_Dictionaries/20_fav_lang.py
@@ -0,0 +1,14 @@
+# A Dictionary of Similar Objects --
+# 127
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+print("Sarah's favorite language is " + favorite_languages['sarah'].title() + ".")
+
+
+# O/p:
+# Sarah's favorite language is C.
