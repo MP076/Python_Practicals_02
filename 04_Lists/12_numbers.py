bf7105c577cc12e954b17fa90253a171027c1efc
Praveen MP <praveen.mpp007@gmail.com>
2020-04-12 16:11:47 +0530

    range() function

---
+++ 04_Lists/12_numbers.py
@@ -0,0 +1,31 @@
+# Using the range() Function --
+# 91
+for value in range(1, 5):
+    print(value)  # It is within 1 to 5, so 5 its excluded.
+print("\n")
+
+# 92
+for value in range(1, 6):
+    print(value)
+print("\n")
+
+
+# Using range() to Make a List of Numbers --
+# 93
+numbers = list(range(1, 6))
+print(numbers)
+
+
+# O/p:
+# 1
+# 2
+# 3
+# 4
+#
+# 1
+# 2
+# 3
+# 4
+# 5
+#
+# [1, 2, 3, 4, 5]
