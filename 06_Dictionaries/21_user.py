7c68b05e7bab3118173c007de4f3560c9c580fa3
Praveen MP <praveen.mpp007@gmail.com>
2020-04-12 22:03:15 +0530

    looping through all key-value pairs

---
+++ 06_Dictionaries/21_user.py
@@ -0,0 +1,21 @@
+# Looping Through All Key-Value Pairs --
+# 128
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+
+
+# O/p:
+# Key: username
+# Value: efermi
+#
+# Key: first
+# Value: enrico
+#
+# Key: last
+# Value: fermi
