0cfd769756a80677769e5676454f9bab9d452ed6
Praveen MP <praveen.mpp007@gmail.com>
2020-04-13 18:13:15 +0530

    if-else statements

---
+++ 10_Control_Statements/37_voting.py
@@ -0,0 +1,49 @@
+# Simple if Statements
+# 177
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+
+# 178
+# age = 19
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+
+
+# if-else Statements --
+# 179
+# age = 17
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry, you are too young to vote.")
+#     print("Please register to vote as soon as you turn 18!")
+
+# 180
+# if 1 == 1:
+#     print("One is equal to One")
+# else:
+#     print("First if was not True")
+
+# 181
+# if 1 == 2:
+#     print("One is equal to Two")
+# else:
+#     print("First if was not True")
+
+
+# O/p:
+# You are old enough to vote!
+#
+# You are old enough to vote!
+# Have you registered to vote yet?
+#
+# Sorry, you are too young to vote.
+# Please register to vote as soon as you turn 18!
+#
+# One is equal to One
+#
+# First if was not True
+#
