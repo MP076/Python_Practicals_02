dabc569050848f4f3c6e6cc37f7db19d1c16c220
Praveen MP <praveen.mpp007@gmail.com>
2020-04-13 13:20:32 +0530

    inequality and numerical comparison

---
+++ 10_Control_Statements/34_toppings.py
@@ -0,0 +1,18 @@
+# Checking for Inequality --
+# 169
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+
+# Numerical Comparisons --
+# 170
+# age = 18
+# print(age == 18)
+
+
+# O/p:
+# Hold the anchovies!
+#
+# True
