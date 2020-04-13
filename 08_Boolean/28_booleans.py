be25f0dd0651b26b5a9bf16a42f84874e1b2a816
Praveen MP <praveen.mpp007@gmail.com>
2020-04-13 11:45:38 +0530

    example programs on booleans

---
+++ 08_Boolean/28_booleans.py
@@ -0,0 +1,39 @@
+# BOOLEANS
+
+# 1. Booleans are data types that indicate a logical state of True or False.
+# 2. Python also has a placeholder object called None.
+
+
+# 150
+a = True
+print("a =", a)
+print("type(a) =", type(a))
+
+b = False
+print("b =", b)
+print("type(b) =", type(b))
+
+
+# 151
+print("1 > 2 :", 1 > 2)
+print("2 == 2 :", 2 == 2)
+
+
+# We can use None as a placeholder for an object that we don't want to reassign yet:
+# 152
+c = None
+print("c =", c)
+print("type(c) =", type(c))
+
+
+# O/p:
+# a = True
+# type(a) = <class 'bool'>
+# b = False
+# type(b) = <class 'bool'>
+#
+# 1 > 2 : False
+# 2 == 2 : True
+#
+# c = None
+# type(c) = <class 'NoneType'>
