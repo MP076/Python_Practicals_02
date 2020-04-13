09ea86f36d8b986e942ab9bf1bc802e36f07de27
Praveen MP <praveen.mpp007@gmail.com>
2020-04-13 11:56:14 +0530

    how input function works

---
+++ 09_User_Input/29_parrot.py
@@ -0,0 +1,9 @@
+# How the input() Function Works
+# 153
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+
+# O/p:
+# Tell me something, and I will repeat it back to you: tell me something
+# tell me something
