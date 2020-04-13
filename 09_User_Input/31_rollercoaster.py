bf491739bab4aecb80cbb769d7bb155593b84b69
Praveen MP <praveen.mpp007@gmail.com>
2020-04-13 12:12:30 +0530

    exaple on prompts and modulus operator

---
+++ 09_User_Input/31_rollercoaster.py
@@ -0,0 +1,29 @@
+# 159
+height = input("How tall are you, in inches? ")
+height = int(height)
+if height >= 5:
+    print("You're tall enough to ride!")
+else:
+    print("You'll be able to ride when you're a little older.")
+print("\n")
+
+
+# The Modulo Operator --
+# 160
+print(4 % 3)
+print(5 % 3)
+print(6 % 3)
+print(7 % 3)
+
+
+# O/p:
+# How tall are you, in inches? 4
+# You'll be able to ride when you're a little older.
+#
+# How tall are you, in inches? 6
+# You're tall enough to ride!
+#
+# 1
+# 2
+# 0
+# 1
