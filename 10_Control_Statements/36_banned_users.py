26392f3d690a6b1a15982ab0683c6fc7b0648998
Praveen MP <praveen.mpp007@gmail.com>
2020-04-13 17:52:08 +0530

    checking whether a value is in list

---
+++ 10_Control_Statements/36_banned_users.py
@@ -0,0 +1,11 @@
+# Checking Whether a Value Is Not in a List --
+# 176
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(user.title() + ", you can post a response if you wish.")
+
+
+# O/p:
+# Marie, you can post a response if you wish.
