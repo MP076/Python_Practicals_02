ea8feae06755efba9272317db417fef53fc91179
Praveen MP <praveen.mpp007@gmail.com>
2020-04-16 06:29:10 +0530

    passing a list

---
+++ 13_Functions/53_greet_users.py
@@ -0,0 +1,13 @@
+# Passing a List --
+
+
+# 242
+def greet_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
