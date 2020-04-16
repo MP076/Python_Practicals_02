97c088869f7fc64163e1b6648f708aeae1ef7246
Praveen MP <praveen.mpp007@gmail.com>
2020-04-16 06:10:17 +0530

    passing positional arguments

---
+++ 13_Functions/48_greeter.py
@@ -0,0 +1,25 @@
+# Defining a Function --
+
+
+# 226
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello World!")
+
+
+greet_user()
+
+
+# Passing Information to a Function --
+# 227
+# def greet_user(username):
+#     print("Hello, " + username.title() + "!")
+#
+#
+# greet_user('joey')
+
+
+# O/p:
+# Hello World!
+#
+# Hello, Joey!
